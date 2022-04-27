073b17eda632fe2b6cce67189679b21fc6a77cd5
JESUS SALATIEL FRANCISCO DOS SANTOS <20611@pmc.local>
2022-04-27 16:58:07 -0300

    inicio e-comerce

---
+++ resources/views/admin/product.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('title', 'Produto')
+
+@section('content')
+<h1 style="text-align: center"> PRODUTOS </h1>
+<div class="container">
+    <div class="card" style="width: 23rem;">
+        <img src="......." class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+</div>
+
+@endsection
